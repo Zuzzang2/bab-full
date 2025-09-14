31bfb5f0abc25dd01484d94e4a3109664b2562b0
ddenzu <eogks999@naver.com>
2025-09-14 22:08:14 +0900

    [Feat] 회원가입 DTO 유효성 검증 추가

--- backend/src/auth/dto/signup.dto.ts
+++ backend/src/auth/dto/signup.dto.ts
@@ -1,10 +1,10 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class SignupDto {
-    @IsEmail()
+    @IsEmail({}, { message: '올바른 이메일 형식을 입력해주세요.' })
     email: string;
 
-    @IsString()
-    @MinLength(6)
+    @IsString({ message: '비밀번호는 문자열이어야 합니다.' })
+    @MinLength(6, { message: '비밀번호는 최소 6자 이상이어야 합니다.' })
     password: string;
 }
